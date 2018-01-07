3999ae8db4fb542a178962de05fec0b651371bbb
John Wojciechowski <jwojo13@gmail.com>
2018-01-07 16:58:47 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,6 +6,7 @@ class MusicLibraryController
   end
 
   def call
+      until input == exit
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -15,7 +16,6 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    until input == exit
       input = gets.strip
     end
   end
