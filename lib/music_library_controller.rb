a5f3af6dfe2b5564c7aaf02c1f0eef9be190d2e7
John Wojciechowski <jwojo13@gmail.com>
2018-01-07 18:23:45 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -66,6 +66,8 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     input = gets.strip
     artist = Artist.find_by_name(input)
+    artist.songs.sort {|a,b| a.name <=> b.name}.each {|genre|
+    puts "#{index}. #{genre.name}"}
 
   end
 
