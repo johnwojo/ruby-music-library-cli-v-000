7d7ca81fe46336c90d473f093528c4fd5a01e434
John Wojciechowski <jwojo13@gmail.com>
2018-01-07 18:15:23 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -64,7 +64,7 @@ class MusicLibraryController
 
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
-    gets.strip
+    input = gets.strip
   end
 
   def list_songs_by_genre
